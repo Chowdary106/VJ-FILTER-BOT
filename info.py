e0dc0f9a626f6520948173c59a93fdaa28a15c3b
Chowdary <131300937+Chowdary106@users.noreply.github.com>
2025-05-31 21:26:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -166,11 +166,11 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "handicapped-bonnee-chowdary106-cbc2c03a.koyeb.app/")
+URL = environ.get("URL", "https://handicapped-bonnee-chowdary106-cbc2c03a.koyeb.app/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
-RENAME_MODE = bool(environ.get('RENAME_MODE', False)) # Set True or False
+RENAME_MODE = bool(environ.get('RENAME_MODE', true)) # Set True or False
 
 
 # Auto Approve Info : If True Then Bot Approve New Upcoming Join Request Else Not
